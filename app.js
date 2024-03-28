01ed7bef0843721adf4b68525e211c92ddda83d9
hyperded <hyperdeath69@gmail.com>
2024-03-28 19:56:38 +0700

    first commitment lol

---
+++ app.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const mongoose = require('mongoose')
+const express = require('express')
+const app = express()
+const port = 3000
+require("dotenv").config();
+const mongoString = process.env.DATABASE_URL
+mongoose.connect(mongoString);
+const database = mongoose.connection
+const routes = require('./routes/routes');  
+
+
+
+app.use('/api', routes)
+
+database.on('error', (error) => {
+    console.log(error)
+})
+
+database.once('connected', () => {
+    console.log('Database Connected');
+})
+
+
+app.listen(port, portCheck())
+
+app.use(express.json());
+
+function real(req, res){
+    res.writeHead(200, {"Content-Type" : "text/html"})
+    fs.readFile("index.html", function (error, data){
+        if(error){
+            res.writeHead(404)
+            res.write("file not found lol")
+        }else
+        {
+            res.write(data)
+        }
+        res.end()
+
+    })
+}
+function portCheck(error){
+    if(error){
+        console.log("Something went wrong...\n", error)
+
+
+    }
+    else{
+        console.log("Stalking on ", port)
+    }
+}
